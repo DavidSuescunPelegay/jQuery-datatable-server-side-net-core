d4a381c7c577193a124a9d51c1a0631e03170e77
David Suescun Pelegay <dsuescun96@gmail.com>
2019-04-08 21:20:06 +0200

    First commit

---
+++ jQueryDatatableServerSideNetCore22/Extensions/LinqExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace jQueryDatatableServerSideNetCore22.Extensions
+{
+    public static class LinqExtensions
+    {
+        public enum Order
+        {
+            Asc,
+            Desc
+        }
+
+        public static IQueryable<T> OrderByDynamic<T>(
+            this IQueryable<T> query,
+            string orderByMember,
+            Order direction)
+        {
+            var queryElementTypeParam = Expression.Parameter(typeof(T));
+
+            var memberAccess = Expression.PropertyOrField(queryElementTypeParam, orderByMember);
+
+            var keySelector = Expression.Lambda(memberAccess, queryElementTypeParam);
+
+            var orderBy = Expression.Call(
+                typeof(Queryable),
+                direction == Order.Asc ? "OrderBy" : "OrderByDescending",
+                new Type[] { typeof(T), memberAccess.Type },
+                query.Expression,
+                Expression.Quote(keySelector));
+
+            return query.Provider.CreateQuery<T>(orderBy);
+        }
+    }
+}
---
+++ jQueryDatatableServerSideNetCore22/wwwroot/js/app.js
@@ -0,0 +1,68 @@
+﻿$(() => {
+    $.fn.dataTable.moment("DD/MM/YYYY HH:mm:ss");
+    $.fn.dataTable.moment("DD/MM/YYYY");
+
+    $("#test-registers").DataTable({
+        // Design Assets
+        stateSave: true,
+        autoWidth: true,
+        // ServerSide Setups
+        processing: true,
+        serverSide: true,
+        // Paging Setups
+        paging: true,
+        // Searching Setups
+        searching: { regex: true },
+        // Ajax Filter
+        ajax: {
+            url: "/TestRegisters/LoadTable",
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: function (d) {
+                return JSON.stringify(d);
+            }
+        },
+        // Columns Setups
+        columns: [
+            { data: "id" },
+            { data: "name" },
+            { data: "firstSurname" },
+            { data: "secondSurname" },
+            { data: "street" },
+            { data: "phone" },
+            { data: "zipCode" },
+            { data: "country" },
+            { data: "notes" },
+            {
+                data: "creationDate",
+                render: function (data, type, row) {
+                    // If display or filter data is requested, format the date
+                    if (type === "display" || type === "filter") {
+                        return moment(data).format("ddd DD/MM/YYYY HH:mm:ss");
+                    }
+                    // Otherwise the data type requested (`type`) is type detection or
+                    // sorting data, for which we want to use the raw date value, so just return
+                    // that, unaltered
+                    return data;
+                }
+            }
+        ],
+        // Column Definitions
+        columnDefs: [
+            { targets: "no-sort", orderable: false },
+            { targets: "no-search", searchable: false },
+            {
+                targets: "trim",
+                render: function (data, type, full, meta) {
+                    if (type === "display") {
+                        data = strtrunc(data, 10);
+                    }
+
+                    return data;
+                }
+            },
+            { targets: "date-type", type: "date-eu" }
+        ]
+    });
+});
