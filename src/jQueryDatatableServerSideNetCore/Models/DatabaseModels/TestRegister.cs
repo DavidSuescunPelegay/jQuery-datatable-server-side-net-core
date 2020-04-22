c01556bf84249ffbcf3bbd9a15d0ea9c0233ce7c
David Suescun Pelegay <dsuescun96@gmail.com>
2020-04-23 00:23:51 +0200

    Migrated Models

---
+++ src/jQueryDatatableServerSideNetCore/Models/DatabaseModels/TestRegister.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace jQueryDatatableServerSideNetCore.Models.DatabaseModels
+{
+    public class TestRegister
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstSurname { get; set; }
+
+        public string SecondSurname { get; set; }
+
+        public string Street { get; set; }
+
+        public string Phone { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Country { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
