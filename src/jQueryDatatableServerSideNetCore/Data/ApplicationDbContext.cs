affde59f1bc65cba7a08daae9ea0c4eab92922f1
David Suescun Pelegay <dsuescun96@gmail.com>
2020-04-23 00:24:06 +0200

    Migrated Data

---
+++ src/jQueryDatatableServerSideNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using jQueryDatatableServerSideNetCore.Models.DatabaseModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace jQueryDatatableServerSideNetCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestRegister> TestRegisters { get; set; }
+    }
+}
