b12973c740d1fe51e9a11b120ca3076972b03d14
David Suescun Pelegay <dsuescun96@gmail.com>
2021-10-22 23:43:30 +0200

    Added ExportFormat Class

---
+++ src/jQueryDatatableServerSideNetCore/Data/ExportFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jQueryDatatableServerSideNetCore.Data
+{
+    public class ExportFormat
+    {
+        public const string Excel = "excel";
+    }
+}
